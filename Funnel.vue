9c0c542235716e99e9422e582318f7ec1a6e5125
chenwenbo236 <41977466+chenwenbo236@users.noreply.github.com>
2020-05-13 22:11:29 +0800

    Add files via upload

---
+++ Funnel.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="funnel">
+    <div style="width:100%;height: 400px" ref="mapchart">echart</div>
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+
+export default {
+  props: {
+    chartData: {
+      type: Object,
+      default() {
+        return {
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      echart: null,
+      width: 11
+    }
+  },
+  watch: {
+    chartData: {
+      handler() {
+        this.initChart()
+      },
+      deep: true
+    }
+  },
+  mounted() {
+    this.initChart()
+  },
+  methods: {
+    initChart() {
+      if (this.echart) {
+        this.echart.setOption(this.chartData)
+      } else {
+        this.echart = echarts.init(this.$refs.mapchart)
+        this.echart.setOption(this.chartData)
+      }
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
